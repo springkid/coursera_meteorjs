f670a1882830cf1301e174fe5ec14572c021d6d9
sr erisso <works@erisso.com>
2016-02-21 16:35:57 +0100

    Step 6 y final finished. Comments on website details page added

--- assignment/siteace/client/helpers/config.js
+++ assignment/siteace/client/helpers/config.js
@@ -2,3 +2,8 @@
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY'
 });
+
+// arkham-comments-ui config initiial
+Comments.ui.config({
+   template: 'bootstrap' // or ionic, semantic-ui
+});
